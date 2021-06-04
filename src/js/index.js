670009bdc1df5b0340d968ad595f623624357aa0
Nervado <evandroabreudeoliveira@gmail.com>
2021-06-03 21:08:56 -0300

    chore: initial commit

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+(function setup() {
+  const a = document.getElementById("root");
+  a.innerHTML = "<div id='root'>div</div";
+  //const b = document.getElementById("game");
+  //console.log(b);
+  //b.innerText = "content";
+  //console.log(a);
+  console.log("load setup function...");
+})();
+
+//class Game {}
